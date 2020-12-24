28a5500ca428e5dcb4cfdd3e52c844ff0b5f8f40
Javier Pantoja <javierjpc1305@gmail.com>
2020-12-23 18:37:22 -0600

    drafts.html

--- functions/groups/maintenance.js
+++ functions/groups/maintenance.js
@@ -12,6 +12,9 @@ exports.uptPop = functions.region('us-east1').pubsub.schedule('every 24 hours').
                     likes: cook.val().likes,
                     favs: cook.val().favs
                 });
+                db.collection('drafts').doc(itm).update({
+                    pop: cook.val().pop
+                });
             });
         });
         admin.database().ref('uptCook').remove();
