7491a0e94a189b1a03ff79c128e9457620cecd82
Javier Pantoja <javierjpc1305@gmail.com>
2021-01-21 19:06:35 -0600

    dynamic sitemap

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const admin = require('firebase-admin');
 admin.initializeApp({
-    databaseURL: "https://science-cookies.firebaseio.com"
+    databaseURL: "https://science-cookies.firebaseio.com",
+    storageBucket: "science-cookies.appspot.com"
 });
 
 exports.cookieFeatures = require('./groups/cookieFeatures');
@@ -8,3 +9,4 @@ exports.dynCookies = require('./groups/dynCookies');
 exports.maintenance = require('./groups/maintenance');
 exports.publish = require('./groups/publish');
 exports.users = require('./groups/users');
+exports.sitemap = require('./groups/sitemap');
