336a709dc04eeaf118f6c419777be38daa679e26
Javier Pantoja <javierjpc1305@gmail.com>
2021-04-09 14:30:45 -0500

    dynAllCals

--- functions/groups/dynAllCals.js
+++ functions/groups/dynAllCals.js
@@ -3,12 +3,10 @@ const admin = require('firebase-admin');
 const db = admin.firestore();
 
 const express = require('express');
-const engines = require('consolidate');
 const app = express();
 
-app.engine('hbs', engines.handlebars);
 app.set('views', './views');
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 
 app.get('/calendario-astronomico/:year', (req, res) => {
     res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
@@ -25,23 +23,16 @@ app.get('/calendario-astronomico/:year', (req, res) => {
                 res.redirect('http://sciencecookies.net/404');
                 return;
             }
-            let months = [], mString = "";
-            let month = -1;
+            let months = [];
             snap.forEach(doc => {
                 let dat = doc.data();
-                if (month != -1) {
-                    mString += '<div class="dropdown-divider d-md-none"></div>\n';
-                    months.push(mString);
-                }
-                mString = '<a href="' + dat.url + '" class="text-decoration-none text-light"><div class="media mb-3">\n';
-                mString += '<img src="' + dat.picUrl + '" class="align-self-center mr-3" alt="' + dat.title + '" width="64px" height="64px">\n';
-                mString += '<div class="media-body">\n';
-                mString += '<h5 class="mt-0">' + dat.title + '</h5>\n';
-                mString += '<p>' + dat.descriptionShort + '</p>\n';
-                mString += '</div></div></a>\n';
-                months.push(mString);
+                months.push({
+                    url: dat.url,
+                    picUrl: dat.picUrl,
+                    title: dat.title,
+                    descriptionShort: dat.descriptionShort
+                });
             });
-
             res.render('allCalsM', {
                 year: year,
                 months: months
@@ -57,14 +48,10 @@ app.get('/calendario-astronomico', (req, res) => {
     }
     res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
     db.collection('sitemap').doc('1').get().then(snap => {
-        let urls = snap.data().calendars, nUrls = [];
+        let urls = snap.data().calendars, years = [];
         urls.splice(0, 1);
         urls.forEach(url => {
-            if (!url.image) nUrls.push(getY(url.loc));
-        });
-        let years = [];
-        nUrls.forEach(year => {
-            years.push(`<li><a href="calendario-astronomico/${year}" class="btn-link-scckie text-light" style="font-size:larger">${year}</a></li>\n`);
+            if (!url.image) years.push(getY(url.loc));
         });
         res.render('allCalsY', { years: years });
         return;
