2e30291be98dc11c3dc7c41b0f3083240f741067
Javier Pantoja <javierjpc1305@gmail.com>
2021-04-23 21:49:19 -0500

    multilang dynArchTri

--- functions/groups/dynArchive.js
+++ functions/groups/dynArchive.js
@@ -8,39 +8,34 @@ const app = express();
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.get('/archivo/:year/:tri', (req, res) => {
+function renderTri(req, res, lang) {
     res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
     let year = req.params.year, tri = req.params.tri;
-    if (!/^[0-9]{4}$/.test(year) || !(tri == 'ene-mar' || tri == 'abr-jun' || tri == 'jul-sep' || tri == 'oct-dic')) {
+    if (!/^[0-9]{4}$/.test(year) || !(tri == 'ene-mar' || tri == 'abr-jun' || tri == 'jul-sep' || tri == 'oct-dic' || tri == 'jan-mar' || tri == 'apr-jun' || tri == 'oct-dec')) {
         console.log('badUrl');
         res.redirect('http://sciencecookies.net/404');
     } else {
         let bMonth, eMonth, eDay;
-        switch (tri) {
-            case 'ene-mar':
-                bMonth = 'Jan';
-                eMonth = 'Mar';
-                eDay = '31';
-                break;
-            case 'abr-jun':
-                bMonth = 'Apr';
-                eMonth = 'Jun';
-                eDay = '30';
-                break;
-            case 'jul-sep':
-                bMonth = 'Jul';
-                eMonth = 'Sep';
-                eDay = '30';
-                break;
-            case 'oct-dic':
-                bMonth = 'Oct';
-                eMonth = 'Dec';
-                eDay = '31';
-                break;
+        if (tri == 'ene-mar' || tri == 'jan-mar') {
+            bMonth = 'Jan';
+            eMonth = 'Mar';
+            eDay = '31';
+        } else if (tri == 'abr-jun' || tri == 'apr-jun') {
+            bMonth = 'Apr';
+            eMonth = 'Jun';
+            eDay = '30';
+        } else if (tri == 'jul-sep') {
+            bMonth = 'Jul';
+            eMonth = 'Sep';
+            eDay = '30';
+        } else if (tri == 'oct-dic' || tri == 'oct-dec') {
+            bMonth = 'Oct';
+            eMonth = 'Dec';
+            eDay = '31';
         }
         let bDate = admin.firestore.Timestamp.fromDate(new Date(Date.parse(year + bMonth + '1')));
         let eDate = admin.firestore.Timestamp.fromDate(new Date(Date.parse(year + eMonth + eDay)));
-        db.collection('galletas').where('public', '==', true).where('date', '>=', bDate).where('date', '<=', eDate).orderBy('date').get().then(snap => {
+        db.collection('cookies/langs/' + lang).where('public', '==', true).where('published', '>=', bDate).where('published', '<=', eDate).orderBy('published').get().then(snap => {
             if (snap.empty) {
                 console.log('snap.empty');
                 res.redirect('http://sciencecookies.net/404');
@@ -49,80 +44,129 @@ app.get('/archivo/:year/:tri', (req, res) => {
             let months = {}, month, monthn;
             snap.forEach(doc => {
                 let dat = doc.data();
-                let date = dat.date.toDate();
+                let date = dat.published.toDate();
                 if (monthn != date.getMonth()) {
                     monthn = date.getMonth();
-                    switch (monthn) {
-                        case 0:
-                            month = "Enero";
-                            break;
-                        case 1:
-                            month = "Febrero";
-                            break;
-                        case 2:
-                            month = "Marzo";
-                            break;
-                        case 3:
-                            month = "Abril";
-                            break;
-                        case 4:
-                            month = "Mayo";
-                            break;
-                        case 5:
-                            month = "Junio";
-                            break;
-                        case 6:
-                            month = "Julio";
-                            break;
-                        case 7:
-                            month = "Agosto";
-                            break;
-                        case 8:
-                            month = "Septiembre";
-                            break;
-                        case 9:
-                            month = "Octubre";
-                            break;
-                        case 10:
-                            month = "Noviembre";
-                            break;
-                        case 11:
-                            month = "Diciembre";
-                            break;
+                    if (lang == "es") {
+                        switch (monthn) {
+                            case 0:
+                                month = "Enero";
+                                break;
+                            case 1:
+                                month = "Febrero";
+                                break;
+                            case 2:
+                                month = "Marzo";
+                                break;
+                            case 3:
+                                month = "Abril";
+                                break;
+                            case 4:
+                                month = "Mayo";
+                                break;
+                            case 5:
+                                month = "Junio";
+                                break;
+                            case 6:
+                                month = "Julio";
+                                break;
+                            case 7:
+                                month = "Agosto";
+                                break;
+                            case 8:
+                                month = "Septiembre";
+                                break;
+                            case 9:
+                                month = "Octubre";
+                                break;
+                            case 10:
+                                month = "Noviembre";
+                                break;
+                            case 11:
+                                month = "Diciembre";
+                                break;
+                        }
+                    } else if (lang == "en") {
+                        switch (monthn) {
+                            case 0:
+                                month = "January";
+                                break;
+                            case 1:
+                                month = "February";
+                                break;
+                            case 2:
+                                month = "March";
+                                break;
+                            case 3:
+                                month = "April";
+                                break;
+                            case 4:
+                                month = "May";
+                                break;
+                            case 5:
+                                month = "June";
+                                break;
+                            case 6:
+                                month = "July";
+                                break;
+                            case 7:
+                                month = "August";
+                                break;
+                            case 8:
+                                month = "September";
+                                break;
+                            case 9:
+                                month = "October";
+                                break;
+                            case 10:
+                                month = "November";
+                                break;
+                            case 11:
+                                month = "December";
+                                break;
+                        }
                     }
                     months[month] = [];
                 }
-                let newMonth = {
+                let cook = {
                     url: dat.url,
                     picUrl: dat.picUrl,
                     title: dat.title,
-                    descrip: dat.descrip,
+                    descrip: dat.description,
                     dledit: dat.dledit,
                     ledit: "",
                     pub: "",
-                    authrs: dat.authrs
+                    authors: dat.authors
                 }
                 if (dat.dledit) {
                     let dl = dat.ledit.toDate();
-                    newMonth.ledit = dl.getDate() + '/' + (dl.getMonth() + 1) + '/' + dl.getFullYear();
+                    cook.ledit = dl.getDate() + '/' + (dl.getMonth() + 1) + '/' + dl.getFullYear();
                 } else {
-                    let d = dat.date.toDate();
-                    newMonth.pub = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+                    let d = dat.published.toDate();
+                    cook.pub = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
                 }
-                months[month].push(newMonth);
+                months[month].push(cook);
             });
+            console.log(lang);
             res.render('archTrim', {
                 year: year,
-                tri: longTri(tri),
-                triLC: longTri(tri).toLowerCase(),
-                months: months
+                tri: longTri(tri, lang),
+                triLC: longTri(tri, lang).toLowerCase(),
+                months: months,
+                setLang: lang
             });
             return;
         }).catch(err => console.log(err));
     }
+}
+app.get('/archivo/:year/:tri', (req, res) => {
+    return renderTri(req, res, "es");
+});
+app.get('/archive/:year/:tri', (req, res) => {
+    return renderTri(req, res, "en");
 });
 
-app.get('/archivo', (req, res) => {
+function renderArch(req, res, lang) {
     function getYT(url) {
         return {
             year: url.substring(35, 39),
@@ -144,7 +188,7 @@ app.get('/archivo', (req, res) => {
             let lyear = url.year, ltri = url.tri;
             if (lyear != year || ltri != tri) {
                 if (!years[lyear]) years[lyear] = {};
-                years[lyear][ltri] = longTri(ltri);
+                years[lyear][ltri] = longTri(ltri, lang);
             }
             tri = ltri;
             year = lyear;
@@ -152,14 +196,27 @@ app.get('/archivo', (req, res) => {
         res.render('archivo', { years: years });
         return;
     }).catch(err => console.log(err));
+}
+app.get('/archivo', (req, res) => {
+    return renderArch(req, res, "es");
+});
+app.get('/archive', (req, res) => {
+    return renderArch(req, res, "en");
 });
 
 
-function longTri(tri) {
-    if (tri == 'ene-mar') return 'Enero - Marzo';
-    if (tri == 'abr-jun') return 'Abril - Junio';
-    if (tri == 'jul-sep') return 'Julio - Septiembre';
-    return 'Octubre - Noviembre';
+function longTri(tri, lang) {
+    if (lang = "es") {
+        if (tri == 'ene-mar') return 'Enero - Marzo';
+        if (tri == 'abr-jun') return 'Abril - Junio';
+        if (tri == 'jul-sep') return 'Julio - Septiembre';
+        return 'Octubre - Noviembre';
+    } else if (lang == 'en') {
+        if (tri == 'jan-mar') return 'January - March';
+        if (tri == 'apr-jun') return 'April - June';
+        if (tri == 'jul-sep') return 'July - September';
+        return 'October - November';
+    }
 }
 
 
