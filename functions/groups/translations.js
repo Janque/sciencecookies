96a4c588b3c628d54ec4c062a91c5fff8d171b89
Javier Pantoja <javierjpc1305@gmail.com>
2021-04-28 18:40:54 -0500

    translation gen progress

--- functions/groups/translations.js
+++ functions/groups/translations.js
@@ -3,6 +3,25 @@ const admin = require('firebase-admin');
 const db = admin.firestore();
 
 exports.translateFullCookie = functions.region('us-east1').https.onCall(data => {
+    const doc = await db.collection('cookies/langs/'+data.from).doc(data.docId).get();
+    let translation={
+        cont: [
+            {
+                type: "head",
+                title: title,
+                author: [author]
+            },
+            {
+                type: "ref",
+                ref: []
+            }
+        ],
+        title: title,
+        description: "",
+        file: file,
+        fixedCats: [],
+    };
     
-    return data;
+    
+    return db.collection('cookies/langs/'+data.target).doc(data.docId).update(translation);
 });
