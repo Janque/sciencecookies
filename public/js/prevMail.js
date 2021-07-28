fe5e69ceae0a1d4b3b1b97ce34e239ac72508e29
Javier Pantoja <javierjpc1305@gmail.com>
2021-07-28 16:41:43 -0500

    deleting public

--- public/js/prevMail.js
+++
@@ -1,2 +0,0 @@
-firebase.auth().onAuthStateChanged((e=>{let t=firebase.app().functions("us-east1").httpsCallable("publish-modAuth");e?t(e.uid).then((e=>{e.data.mod||(window.location.href="https://sciencecookies.net")})).catch((e=>console.log(e))):window.location.href="https://sciencecookies.net"}));
-//# sourceMappingURL=prevMail.js.map
