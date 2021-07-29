249ef17b1f37e070639f62195aaf62b236f29f02
Javier Pantoja <javierjpc1305@gmail.com>
2021-07-28 19:03:15 -0500

    improve scripts

--- scripts/parcelBuild.sh
+++ scripts/parcelBuild.sh
@@ -1,8 +1,16 @@
 rm functions/views/includes/*
-cp -r src/includes/* functions/views/includes/
+if [ ! -d "/functions/views/includes" ] 
+then
+    mkdir /functions/views/includes
+fi
+cp -r src/includes/* functions/views/includes
 
 rm functions/views/layouts/*
-cp -r src/layouts/* functions/views/layouts/
+if [ ! -d "/functions/views/layouts" ] 
+then
+    mkdir /functions/views/layouts
+fi
+cp -r src/layouts/* functions/views/layouts
 
 rm -r public
 
