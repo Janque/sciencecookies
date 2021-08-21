8cc768dd4445f665d55d4d53769f39894425cb88
Javier Pantoja <javierjpc1305@gmail.com>
2021-08-21 17:42:09 -0500

    firebase initialization

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,3 +1,24 @@
+import { initializeApp, getApps, getApp } from "firebase/app"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCc5LmjPpufLuHzR6RiXR7awOdGuWpztTk",
+    authDomain: "sciencecookies.net",
+    databaseURL: "https://science-cookies.firebaseio.com",
+    projectId: "science-cookies",
+    storageBucket: "science-cookies.appspot.com",
+    messagingSenderId: "906770471712",
+    appId: "1:906770471712:web:c7a2c16bac19b6c2d7d545",
+    measurementId: "G-1MYVREMBFV"
+};
+
+var firebaseApp;
+if (!getApps().length) {
+    firebaseApp = initializeApp(firebaseConfig);
+}
+else {
+    firebaseApp = getApp();
+}
+
 import { getDatabase, ref, set, increment } from "firebase/database";
 const RTDB = getDatabase();
 
--- src/js/edit.js
+++ src/js/edit.js
@@ -1,8 +1,29 @@
+import { initializeApp, getApps, getApp } from "firebase/app"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCc5LmjPpufLuHzR6RiXR7awOdGuWpztTk",
+    authDomain: "sciencecookies.net",
+    databaseURL: "https://science-cookies.firebaseio.com",
+    projectId: "science-cookies",
+    storageBucket: "science-cookies.appspot.com",
+    messagingSenderId: "906770471712",
+    appId: "1:906770471712:web:c7a2c16bac19b6c2d7d545",
+    measurementId: "G-1MYVREMBFV"
+};
+
+var firebaseApp;
+if (!getApps().length) {
+    firebaseApp = initializeApp(firebaseConfig);
+}
+else {
+    firebaseApp = getApp();
+}
+
 import { getDatabase, ref, set } from "firebase/database";
 const RTDB = getDatabase();
 
 import { getFunctions, httpsCallable } from "firebase/functions";
-const FUNCTIONS = getFunctions();
+const FUNCTIONS = getFunctions(firebaseApp, 'us-east1');
 
 import { getFirestore, getDoc, doc as docRef, getDocs, onSnapshot, updateDoc, query } from "firebase/firestore";
 const FSDB = getFirestore();
