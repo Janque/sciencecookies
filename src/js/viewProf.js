47f7067cef542f22b9be5d9806ef76b68b4ed50b
Javier Pantoja <javierjpc1305@gmail.com>
2021-09-01 22:09:13 -0500

    remove usersPublic viewProf

--- src/js/viewProf.js
+++ src/js/viewProf.js
@@ -26,10 +26,7 @@ import { getStorage, ref as storageRef, getDownloadURL } from "firebase/storage"
 const STORAGE = getStorage();
 
 //Global
-var points, rank, gotFL = false;
-var favn, favl, likedn, likedl;
-var ppic, pname, pemail;
-var visible, vemail, vfl;
+var userPublic, gotFL = false;
 
 var urlSrch;
 window.loaded = function loaded() {
@@ -40,39 +37,39 @@ window.loaded = function loaded() {
 function shwPrfl() {
     document.getElementById('navBtnPrfl').classList.add('active');
     getDownloadURL(storageRef(STORAGE, 'ppics/' + urlSrch.get('u') + '/pp_200x200')).then(url => {
-        if (ppic != null) document.getElementById('disPP').setAttribute('onerror', "this.src='" + ppic + "'");
+        if (userPublic.ppic != null) document.getElementById('disPP').setAttribute('onerror', "this.src='" + userPublic.ppic + "'");
         else document.getElementById('disPP').setAttribute('onerror', "this.src='https://via.placeholder.com/20.webp'");
         document.getElementById('disPP').src = url;
-        if (pemail != null) document.getElementById('contEmail').classList.remove('d-none');
-        document.getElementById('disMail').innerHTML = pemail;
-        document.getElementById('disName').innerHTML = pname;
+        if (userPublic.pemail != null) document.getElementById('contEmail').classList.remove('d-none');
+        document.getElementById('disMail').innerHTML = userPublic.pemail;
+        document.getElementById('disName').innerHTML = userPublic.pname;
         document.getElementById('crdPrfl').classList.remove('d-none');
     }).catch(err => {
-        if (ppic != null) document.getElementById('disPP').setAttribute('onerror', "this.src='" + ppic + "'");
+        if (userPublic.ppic != null) document.getElementById('disPP').setAttribute('onerror', "this.src='" + userPublic.ppic + "'");
         else document.getElementById('disPP').setAttribute('onerror', "https://via.placeholder.com/20.webp'");
         document.getElementById('disPP').src = '';
-        if (pemail != null) document.getElementById('contEmail').classList.remove('d-none');
-        document.getElementById('disMail').innerHTML = pemail;
-        document.getElementById('disName').innerHTML = pname;
+        if (userPublic.pemail != null) document.getElementById('contEmail').classList.remove('d-none');
+        document.getElementById('disMail').innerHTML = userPublic.pemail;
+        document.getElementById('disName').innerHTML = userPublic.pname;
         document.getElementById('crdPrfl').classList.remove('d-none');
     });
 }
 function shwFav() {
-    if (favn == null) return;
+    if (userPublic.favn == null) return;
     document.getElementById('navBtnFav').classList.add('active');
     let favStr = '';
-    favn.forEach(function (itm, idx) {
-        favStr = favStr + '<li class="list-group-item text-light border-light bg-transparent"><a class="text-decoration-none text-light" href="galletas/' + favl[idx] + '">' + itm + ' <i class="fas fa-link"></i></a></li>';
+    userPublic.favn.forEach(function (itm, idx) {
+        favStr = favStr + '<li class="list-group-item text-light border-light bg-transparent"><a class="text-decoration-none text-light" href="galletas/' + userPublic.favl[idx] + '">' + itm + ' <i class="fas fa-link"></i></a></li>';
     });
     document.getElementById('cntFav').innerHTML = favStr;
     document.getElementById('crdFav').classList.remove('d-none');
 }
 function shwLike() {
-    if (likedn == null) return;
+    if (userPublic.likedn == null) return;
     document.getElementById('navBtnLike').classList.add('active');
     let likedStr = '';
-    likedn.forEach(function (itm, idx) {
-        likedStr = likedStr + '<li class="list-group-item text-light border-light bg-transparent"><a class="text-decoration-none text-light" href="galletas/' + likedl[idx] + '">' + itm + ' <i class="fas fa-link"></i></a></li>';
+    userPublic.likedn.forEach(function (itm, idx) {
+        likedStr = likedStr + '<li class="list-group-item text-light border-light bg-transparent"><a class="text-decoration-none text-light" href="galletas/' + userPublic.likedl[idx] + '">' + itm + ' <i class="fas fa-link"></i></a></li>';
     });
     document.getElementById('cntLike').innerHTML = likedStr;
     document.getElementById('crdLike').classList.remove('d-none');
@@ -81,11 +78,11 @@ function shwCrds(t, u) {
     if ((u == null || u == '') && !gotFL) {
         window.location.href = 'https://sciencecookies.net';
     }
-    if (t == null || favn == null) t = 'prof';
+    if (t == null || userPublic.favn == null) t = 'prof';
     if (gotFL == false) {
         const getUserPublic = httpsCallable(FUNCTIONS, 'users-getUserPublic');
-        getUserPublic(u).then(userPublic => {
-            userPublic = userPublic.data;
+        getUserPublic(u).then(res => {
+            userPublic = res.data;
             if (userPublic == null) {
                 document.getElementById('navBtnFav').classList.add('disabled');
                 document.getElementById('navBtnLike').classList.add('disabled');
@@ -95,25 +92,13 @@ function shwCrds(t, u) {
                 document.getElementById('contNull').innerHTML = '<div class="col"><strong>Este perfíl no existe o es privado</strong></div>';
                 document.getElementById('crdPrfl').classList.remove('d-none');
             } else {
-                favn = userPublic.favn;
-                favl = userPublic.favl;
-                likedn = userPublic.likedn;
-                likedl = userPublic.likedl;
-                points = userPublic.points;
-                rank = userPublic.rank;
-                visible = userPublic.visible;
-                vemail = userPublic.vemail;
-                vfl = userPublic.vfl;
-                ppic = userPublic.ppic;
-                pemail = userPublic.pemail;
-                pname = userPublic.pname;
-                if (favn == null) {
+                if (userPublic.favn == null) {
                     document.getElementById('navBtnFav').classList.add('disabled');
                     document.getElementById('navBtnLike').classList.add('disabled');
                 }
                 shwCrds2(t);
             }
-        }).catch(err => { });
+        }).catch(err => { console.log('err') });
         gotFL = true;
     } else {
         shwCrds2(t);
@@ -121,7 +106,7 @@ function shwCrds(t, u) {
 }
 function shwCrds2(t) {
     if (t == 'favs') {
-        if (favn == null) return;
+        if (userPublic.favn == null) return;
         document.getElementById('crdPrfl').classList.add('d-none');
         document.getElementById('navBtnPrfl').classList.remove('active');
         document.getElementById('crdLike').classList.add('d-none');
@@ -129,7 +114,7 @@ function shwCrds2(t) {
         shwFav();
     } else {
         if (t == 'likes') {
-            if (likedn == null) return;
+            if (userPublic.likedn == null) return;
             document.getElementById('crdFav').classList.add('d-none');
             document.getElementById('navBtnFav').classList.remove('active');
             document.getElementById('crdPrfl').classList.add('d-none');
