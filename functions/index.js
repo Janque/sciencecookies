d545bc3acbf96cd48580d630273c7541b915c05b
Javier Pantoja <javierjpc1305@gmail.com>
2022-10-16 13:25:05 +0200

    setup

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ admin.initializeApp({
     storageBucket: "science-cookies.appspot.com"
 });
 
-exports.cookieFeatures = require('./groups/cookieFeatures');
+/*exports.cookieFeatures = require('./groups/cookieFeatures');
 exports.dynCookies = require('./groups/dynCookies');
 exports.dynCalendars = require('./groups/dynCalendars');
 exports.dynAllCals = require('./groups/dynAllCals');
@@ -15,3 +15,4 @@ exports.publish = require('./groups/publish');
 exports.sitemap = require('./groups/sitemap');
 exports.translations = require('./groups/translations');
 exports.users = require('./groups/users');
+*/
