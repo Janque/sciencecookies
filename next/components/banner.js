54f55eab998a5930b56a3f799b66db935c7df861
Javier Pantoja <javierjpc1305@gmail.com>
2022-11-09 16:48:39 +0100

    img sizes

--- next/components/banner.js
+++ next/components/banner.js
@@ -6,7 +6,7 @@ export default function Banner() {
     return (
         <div className="row justify-content-center mt-3 mb-4 align-items-center" style={{ height: 'calc(100% - 2.5rem)' }}>
             <div className="col-10 col-sm-5 col-xl-3" style={{ height: '100%' }}>
-                <Image priority fill src='/img/logoT.svg' alt='Science Cookies Logo' />
+                <Image priority fill src='/img/logoT.svg' alt='Science Cookies Logo' sizes="(max-width: 576px) 85vw, (max-width: 1200px) 42vw, 25vw" />
             </div>
             <div className="col-12 col-sm-6 col-xl-4">
                 <h1 className="title v1 text-center" style={{ fontSize: '2.8rem' }}>SCIENCE COOKIES</h1>
