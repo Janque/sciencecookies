2af3c188f532d4b9849935fe9a3cccde87e0d0be
Javier Pantoja <javierjpc1305@gmail.com>
2022-11-12 14:14:43 +0100

    fix banner in mobile

--- next/components/banner.js
+++ next/components/banner.js
@@ -4,9 +4,11 @@ import Image from 'next/image';
 export default function Banner() {
     const router = useRouter();
     return (
-        <div className="row justify-content-center mt-3 mb-4 align-items-center" style={{ height: 'calc(100% - 2.5rem)' }}>
-            <div className="col-10 col-sm-5 col-xl-3" style={{ height: '100%' }}>
-                <Image priority fill src='/img/logoT.svg' alt='Science Cookies Logo' sizes="(max-width: 576px) 85vw, (max-width: 1200px) 42vw, 25vw" />
+        <div className="row justify-content-center mt-3 mb-4 align-items-center">
+            <div className="col-10 col-sm-5 col-xl-3">
+                <div style={{ position: 'relative', width: '100%', paddingTop: '100%' }}>
+                    <Image priority fill src='/img/logoT.svg' alt='Science Cookies Logo' sizes="(max-width: 576px) 85vw, (max-width: 1200px) 42vw, 25vw" />
+                </div>
             </div>
             <div className="col-12 col-sm-6 col-xl-4">
                 <h1 className="title v1 text-center" style={{ fontSize: '2.8rem' }}>SCIENCE COOKIES</h1>
